0b2ccc514a081e9516cc45184253843d6321620e
Ayush Raj <ayush.raj138@gmail.com>
2023-07-29 11:11:04 +0530

    fix

--- src/utils/dataReducer.js
+++ src/utils/dataReducer.js
@@ -1,5 +1,6 @@
 export const initialData={};
 export const dataReducer=(state,action)=>{
+    //eslint-disable-next-line
     const {type,payload}=action;
     switch(type){
         default: return state;
